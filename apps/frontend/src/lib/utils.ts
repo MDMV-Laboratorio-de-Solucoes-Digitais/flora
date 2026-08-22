8e142c15c2527a230a239dc5ca8b08fbbad0f34d
Luis <lfgranjaead@gmail.com>
2026-08-22 09:06:27 -0400

    feat(frontend): add cn class helper utility

---
+++ apps/frontend/src/lib/utils.ts
@@ -0,0 +1,11 @@
+/* eslint-disable unicorn/name-replacements */
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+export type WithElementRef<T> = T & { ref?: HTMLElement | null };
+export type WithoutChild<T> = Omit<T, 'child'>;
+export type WithoutChildrenOrChild<T> = Omit<T, 'children' | 'child'>;
